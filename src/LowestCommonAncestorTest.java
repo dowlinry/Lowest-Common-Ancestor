991a6c1332c0fd48adbcffccf34467e891fe0a24
dowlinry <dowlinry@tcd.ie>
2019-09-19 10:59:24 +0100

    Added tests for all left tree and tree with random nodes

--- src/LowestCommonAncestorTest.java
+++ src/LowestCommonAncestorTest.java
@@ -21,4 +21,28 @@ class LowestCommonAncestorTest {
 		assertEquals("This answer should be 2", 2 ,testTree.findLCA(2, 3));
 	}
 
+	@Test
+	void testAllLeftTree() {
+		LowestCommonAncestor testTree = new LowestCommonAncestor();
+		testTree.root = new Node(0);
+		testTree.root.left = new Node(1);
+		testTree.root.left.left = new Node(2);
+		testTree.root.left.left.left = new Node(3);
+		assertEquals("This answer should be 1", 1 ,testTree.findLCA(1, 3));
+	}
+	
+	@Test
+	void testTree1() {
+        LowestCommonAncestor testTree = new LowestCommonAncestor(); 
+        testTree.root = new Node(0); 
+        testTree.root.left = new Node(1); 
+        testTree.root.right = new Node(2); 
+        testTree.root.left.left = new Node(3); 
+        testTree.root.left.right = new Node(4); 
+        testTree.root.right.left = new Node(5); 
+        testTree.root.right.right = new Node(6);
+        assertEquals("This answer should be 0",0,testTree.findLCA(1, 5));
+  
+	}
+
 }
